6eda60ddb89018782f25b085f694704fe326edf9
framussi <francesco.mussi@hotmail.it>
2025-11-09 14:02:55 +0200

    Update fourthway.js

--- quotes/fourthway.js
+++ quotes/fourthway.js
@@ -1,52 +1,155 @@
 export const fourthway = [
+  [
   {
+    "quote": "Study of internal considering, of mechanicalness, of lying, of imagination, of identification ... first you must scrub the machine clean; it is too covered with rust.",
     "author": "P. D. Ouspensky",
-    "book": "The Fourth Way",
-    "quote": "Study of internal considering, of mechanicalness, of lying, of imagination, of identification shows that they all belong to us, that we are always in these states. When you see this, you realize the difficulty of work on oneself. Such as you are you cannot begin to get something new; you will see that first you must scrub the machine clean; it is too covered with rust."
+    "book": "The Fourth Way"
   },
   {
+    "quote": "Identification is a necessary element in every negative emotion ... when you separate identification and imagination, the negative emotion practically disappears.",
     "author": "P. D. Ouspensky",
-    "book": "The Fourth Way",
-    "quote": "Identification is a necessary element in every negative emotion; you cannot have a negative emotion without identification. So the study of identification and struggle against identification are very powerful weapons in the struggle with negative emotions. Sometimes you cannot struggle with a negative emotion directly, but by trying not to identify you diminish its power, for all the power of negative emotions lies in identification and imagination."
+    "book": "The Psychology of Man's Possible Evolution"
   },
   {
+    "quote": "Q. I had a problem which was worrying me ... This quiet place is not a metaphor—it is a very real thing.",
     "author": "P. D. Ouspensky",
-    "book": "The Fourth Way",
-    "quote": "Continue to observe and you will find that there is a place in you where you are quiet, calm, and nothing can disturb you—only it is difficult to find the way there. But if you do it several times you will be able to remember some of the steps, and by the same steps you may come there again. This quiet place is not a metaphor—it is a very real thing."
+    "book": "The Fourth Way"
   },
   {
+    "quote": "As it is, almost every idea given remains unopened, unexplored. There are boxes and boxes ... It is not enough simply to sit on those boxes of books.",
     "author": "P. D. Ouspensky",
-    "book": "The Fourth Way",
-    "quote": "As it is, almost every idea given remains unopened, unexplored. There are boxes and boxes that may be opened, the contents read and many new things added. But mostly we deal with unopened boxes. One box—knowledge, another—being, a third—understanding, and so on. We do not even open the boxes."
+    "book": "The Fourth Way"
   },
   {
+    "quote": "It is necessary always to remember why you started. Do you want to get things you can get from ordinary life or different things?",
     "author": "P. D. Ouspensky",
-    "book": "The Fourth Way",
-    "quote": "By dividing yourself, by not saying 'I' to everything. You can really use the word 'I' only in relation to the more conscious part of yourself—desire to work, desire to understand, realization of not understanding, realization of mechanicalness; that you can call 'I'. 'I' starts growing only in connection with study, with work on oneself; otherwise it cannot grow and there is no change."
+    "book": "The Fourth Way"
   },
   {
+    "quote": "Q. Do you mean we must study our false personality by collecting material, observations? A. By dividing yourself, by not saying 'I' to everything ... real 'I' gradually grows stronger and stronger, chiefly through self-remembering.",
     "author": "P. D. Ouspensky",
-    "book": "The Fourth Way",
-    "quote": "Do all negative emotions spring from false personality? How could it be otherwise? It is, so to speak, a special organ for negative emotions, for displaying, enjoying and producing them. You remember that there is no real centre for negative emotions. False personality acts as a centre for them."
+    "book": "The Fourth Way"
   },
   {
+    "quote": "Self-remembering is a thing that must be based on right function ... All are necessary for creating this right combination.",
     "author": "P. D. Ouspensky",
-    "book": "The Fourth Way",
-    "quote": "The only thing one can seriously think about when one realizes that one is in prison is how to escape, not sit and cry about injustice in prison."
+    "book": "The Fourth Way"
   },
   {
+    "quote": "Q. I find it difficult to see the difference between 'I' and 'Miss B.' ... then, at a certain moment, it will appear and stay long enough for you to recognize and remember it.",
     "author": "P. D. Ouspensky",
-    "book": "The Fourth Way",
-    "quote": "Effort is our money. If we want something, we must pay with effort. According to the strength of effort and the time of effort—in the sense of whether it is the right time for effort or not—we obtain results."
+    "book": "The Fourth Way"
   },
   {
+    "quote": "Q. Do all negative emotions spring from false personality? A. How could it be otherwise? ... False personality acts as a centre for them.",
     "author": "P. D. Ouspensky",
-    "book": "The Fourth Way",
-    "quote": "Realization of one's present state. When one realizes that one deceives oneself, that one is asleep and one's house is on fire, always, permanently on fire, and that it is only by accident that the fire has not reached one's room at this very moment, when one realizes this, one will want to make efforts to awake and one will not expect any special reward."
+    "book": "The Fourth Way"
   },
   {
+    "quote": "You must not trust your false personality—its ideas, its words, its actions. You cannot destroy it, but you can make it passive ...",
     "author": "P. D. Ouspensky",
-    "book": "The Fourth Way",
-    "quote": "Who remembers the aim, who wants to work is 'I', the rest is 'Ouspensky'."
+    "book": "The Fourth Way"
+  },
+  {
+    "quote": "The beginning of self-knowledge is understanding who speaks in you and whom you can trust.",
+    "author": "P. D. Ouspensky",
+    "book": "The Fourth Way"
+  },
+  {
+    "quote": "The only thing one can seriously think about when one realizes that one is in prison is how to escape, not sit and cry about injustice in prison.",
+    "author": "P. D. Ouspensky",
+    "book": "The Fourth Way"
+  },
+  {
+    "quote": "Q. I am one of those people who only learn five words a day ... This laziness must be overcome.",
+    "author": "P. D. Ouspensky",
+    "book": "The Fourth Way"
+  },
+  {
+    "quote": "In the work every moment one has to overcome laziness ... We are surrounded by neglected things.",
+    "author": "P. D. Ouspensky",
+    "book": "The Fourth Way"
+  },
+  {
+    "quote": "Effort is our money. If we want something, we must pay with effort ... we obtain results.",
+    "author": "P. D. Ouspensky",
+    "book": "The Fourth Way"
+  },
+  {
+    "quote": "The first thing you have to decide is to do your own work and to do it regularly ... You must try not to forget them.",
+    "author": "P. D. Ouspensky",
+    "book": "The Fourth Way"
+  },
+  {
+    "quote": "Q. What creates stimulus for work? A. Realization of one's present state ... one’s house is on fire ...",
+    "author": "P. D. Ouspensky",
+    "book": "The Fourth Way"
+  },
+  {
+    "quote": "There is nothing in life that cannot become work if one tries to remember oneself ...",
+    "author": "P. D. Ouspensky",
+    "book": "The Fourth Way"
+  },
+  {
+    "quote": "In a state of identification they are completely shut out: one small 'I' occupies the whole field.",
+    "author": "P. D. Ouspensky",
+    "book": "The Fourth Way"
+  },
+  {
+    "quote": "Q. What is it in us that makes efforts? A. Again one of the 'I's or a small group of them ...",
+    "author": "P. D. Ouspensky",
+    "book": "The Fourth Way"
+  },
+  {
+    "quote": "Q. I see now, at this moment, that I do not remember myself, but it goes no further. A. Go deeper ...",
+    "author": "P. D. Ouspensky",
+    "book": "The Fourth Way"
+  },
+  {
+    "quote": "It is necessary to remember all that was said at different times about effort ... We want very big things ...",
+    "author": "P. D. Ouspensky",
+    "book": "The Fourth Way"
+  },
+  {
+    "quote": "Even in the right way one can make efforts for some time and then, if one stops, all those efforts will be lost.",
+    "author": "P. D. Ouspensky",
+    "book": "The Fourth Way"
+  },
+  {
+    "quote": "Q. What explains the sense of urgency created in oneself in a better state? A. In the beginning it is magnetic centre ...",
+    "author": "P. D. Ouspensky",
+    "book": "The Fourth Way"
+  },
+  {
+    "quote": "Many things get forgotten and become dull because we forget the starting-point.",
+    "author": "P. D. Ouspensky",
+    "book": "The Fourth Way"
+  },
+  {
+    "quote": "All things come through friction, friction between the place where 'I' can grow and false personality.",
+    "author": "P. D. Ouspensky",
+    "book": "The Fourth Way"
+  },
+  {
+    "quote": "Who remembers the aim, who wants to work is 'I', the rest is 'Ouspensky'.",
+    "author": "P. D. Ouspensky",
+    "book": "The Fourth Way"
+  },
+  {
+    "quote": "False personality decides everything ... sometimes it happens that false personality may disappear for ten or fifteen minutes ...",
+    "author": "P. D. Ouspensky",
+    "book": "The Fourth Way"
+  },
+  {
+    "quote": "Q. I find I am more aware of myself when alone ... The best time to try to remember yourself is when circumstances are most difficult ...",
+    "author": "P. D. Ouspensky",
+    "book": "The Fourth Way"
+  },
+  {
+    "quote": "Little by little you will learn to connect it with more and more things and after some time ideas of the system will enter into everything.",
+    "author": "P. D. Ouspensky",
+    "book": "The Fourth Way"
   }
+]
+
 ]
