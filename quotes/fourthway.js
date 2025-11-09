34014c17e4ee5fb93a17d1503d3f50dbd7901cf6
framussi <francesco.mussi@hotmail.it>
2025-11-09 13:59:26 +0200

    Create fourthway.js

---
+++ quotes/fourthway.js
@@ -0,0 +1,52 @@
+export const fourthway = [
+  {
+    "author": "P. D. Ouspensky",
+    "book": "The Fourth Way",
+    "quote": "Study of internal considering, of mechanicalness, of lying, of imagination, of identification shows that they all belong to us, that we are always in these states. When you see this, you realize the difficulty of work on oneself. Such as you are you cannot begin to get something new; you will see that first you must scrub the machine clean; it is too covered with rust."
+  },
+  {
+    "author": "P. D. Ouspensky",
+    "book": "The Fourth Way",
+    "quote": "Identification is a necessary element in every negative emotion; you cannot have a negative emotion without identification. So the study of identification and struggle against identification are very powerful weapons in the struggle with negative emotions. Sometimes you cannot struggle with a negative emotion directly, but by trying not to identify you diminish its power, for all the power of negative emotions lies in identification and imagination."
+  },
+  {
+    "author": "P. D. Ouspensky",
+    "book": "The Fourth Way",
+    "quote": "Continue to observe and you will find that there is a place in you where you are quiet, calm, and nothing can disturb you—only it is difficult to find the way there. But if you do it several times you will be able to remember some of the steps, and by the same steps you may come there again. This quiet place is not a metaphor—it is a very real thing."
+  },
+  {
+    "author": "P. D. Ouspensky",
+    "book": "The Fourth Way",
+    "quote": "As it is, almost every idea given remains unopened, unexplored. There are boxes and boxes that may be opened, the contents read and many new things added. But mostly we deal with unopened boxes. One box—knowledge, another—being, a third—understanding, and so on. We do not even open the boxes."
+  },
+  {
+    "author": "P. D. Ouspensky",
+    "book": "The Fourth Way",
+    "quote": "By dividing yourself, by not saying 'I' to everything. You can really use the word 'I' only in relation to the more conscious part of yourself—desire to work, desire to understand, realization of not understanding, realization of mechanicalness; that you can call 'I'. 'I' starts growing only in connection with study, with work on oneself; otherwise it cannot grow and there is no change."
+  },
+  {
+    "author": "P. D. Ouspensky",
+    "book": "The Fourth Way",
+    "quote": "Do all negative emotions spring from false personality? How could it be otherwise? It is, so to speak, a special organ for negative emotions, for displaying, enjoying and producing them. You remember that there is no real centre for negative emotions. False personality acts as a centre for them."
+  },
+  {
+    "author": "P. D. Ouspensky",
+    "book": "The Fourth Way",
+    "quote": "The only thing one can seriously think about when one realizes that one is in prison is how to escape, not sit and cry about injustice in prison."
+  },
+  {
+    "author": "P. D. Ouspensky",
+    "book": "The Fourth Way",
+    "quote": "Effort is our money. If we want something, we must pay with effort. According to the strength of effort and the time of effort—in the sense of whether it is the right time for effort or not—we obtain results."
+  },
+  {
+    "author": "P. D. Ouspensky",
+    "book": "The Fourth Way",
+    "quote": "Realization of one's present state. When one realizes that one deceives oneself, that one is asleep and one's house is on fire, always, permanently on fire, and that it is only by accident that the fire has not reached one's room at this very moment, when one realizes this, one will want to make efforts to awake and one will not expect any special reward."
+  },
+  {
+    "author": "P. D. Ouspensky",
+    "book": "The Fourth Way",
+    "quote": "Who remembers the aim, who wants to work is 'I', the rest is 'Ouspensky'."
+  }
+]
