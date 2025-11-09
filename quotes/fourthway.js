8b3b372cb9fb13adf606c970c69727d0c95feb47
framussi <francesco.mussi@hotmail.it>
2025-11-09 19:46:00 +0200

    Update fourthway.js

--- quotes/fourthway.js
+++ quotes/fourthway.js
@@ -5,9 +5,9 @@ export const fourthway = [
     "book": "The Fourth Way"
   },
   {
-    "quote": "Identification is a necessary element in every negative emotion ... when you separate identification and imagination, the negative emotion practically disappears.",
+    "quote": "Identification is a necessary element in every negative emotion... when you separate identification and imagination, the negative emotion practically disappears.",
     "author": "P. D. Ouspensky",
-    "book": "The Psychology of Man's Possible Evolution"
+    "book": "The Fourth Way"
   },
   {
     "quote": "Q. I had a problem which was worrying me ... This quiet place is not a metaphor—it is a very real thing.",
@@ -135,12 +135,12 @@ export const fourthway = [
     "book": "The Fourth Way"
   },
   {
-    "quote": "False personality decides everything ... sometimes it happens that false personality may disappear for ten or fifteen minutes ...",
+    "quote": "False personality decides everything... sometimes it happens that false personality may disappear for ten or fifteen minutes ...",
     "author": "P. D. Ouspensky",
     "book": "The Fourth Way"
   },
   {
-    "quote": "Q. I find I am more aware of myself when alone ... The best time to try to remember yourself is when circumstances are most difficult ...",
+    "quote": "Q. I find I am more aware of myself when alone... The best time to try to remember yourself is when circumstances are most difficult ...",
     "author": "P. D. Ouspensky",
     "book": "The Fourth Way"
   },
@@ -148,5 +148,75 @@ export const fourthway = [
     "quote": "Little by little you will learn to connect it with more and more things and after some time ideas of the system will enter into everything.",
     "author": "P. D. Ouspensky",
     "book": "The Fourth Way"
+  },
+  {
+    "author": "P. D. Ouspensky",
+    "book": "The Fourth Way",
+    "quote": "Q. I have a certain critical attitude to people I see a lot and I tried to stop it, but it has come back again very badly. A. Yes, sometimes it can be a very oppressive thing and more difficult to stop than people think. There is only one thing—just to look at it from the point of view of personal profit. Does this critical attitude give you anything or not? You will see that it gives you nothing. We often forget this question of personal profit, yet it is not only legitimate, it is the only criterion. Sometimes we spend enormous efforts, time and emotion on things from which we can get no benefit. Perhaps this will help you not to criticize. It is just the same as criticizing the weather."
+  },
+  {
+    "author": "P. D. Ouspensky",
+    "book": "The Fourth Way",
+    "quote": "The most important moments to try to remember oneself are moments when one is habitually most identified, for if one can remember oneself at these difficult moments other moments will be easier. But if one chooses only the easiest moments, then at difficult moments one will never be able to do it. It is necessary to try both."
+  },
+  {
+    "author": "P. D. Ouspensky",
+    "book": "The Fourth Way",
+    "quote": "You can train yourself in such a way that negative emotions will remind you of the necessity of remembering yourself."
+  },
+  {
+    "author": "P. D. Ouspensky",
+    "book": "The Fourth Way",
+    "quote": "Q. How can I think rightly about self-pity? A. In thinking about it you must create in your mind a permanent solvent, an attitude which will break up your self-pity when it appears. If you succeed in forming a permanent attitude, then as soon as self-pity appears, pour the solvent over it and it will dissolve."
+  },
+  {
+    "author": "P. D. Ouspensky",
+    "book": "The Fourth Way",
+    "quote": "Q. Can conquering negative emotions, like fear, create energy? A. Very much so. This is one of the best means of collecting energy. All possibilities of development are contained in conquering negative emotions and transforming them. A man with negative emotions will never do anything."
+  },
+  {
+    "author": "P. D. Ouspensky",
+    "book": "The Fourth Way",
+    "quote": "Q. Into what kind of things can negative emotions be transformed? A. It is better to say they must be conquered. But, if you like, we can say that they are transformed into some kind of emotion mixed with very much understanding—an emotion of higher parts of centres. Almost any negative emotion we have now can be transformed into an emotion of higher parts of centres. But this needs understanding, conviction that it is necessary, and decision to do it."
+  },
+  {
+    "author": "P. D. Ouspensky",
+    "book": "The Fourth Way",
+    "quote": "Q. I find I become very negative when given advice and never agree with it. A. Again it is all based on your attitude: you admit that someone else can affect your emotional state. We should not admit an emotional power like that over us. You must stop justifying it in your mind. When one stops this justifying, one very often finds that the cause of all these negative emotions is some wrong idea."
+  },
+  {
+    "author": "P. D. Ouspensky",
+    "book": "The Fourth Way",
+    "quote": "If we manage to remove identification, the same thing that we know now as irritation becomes a very useful emotion, a kind of tentacle by which we can feel mechanicalness. You cannot imagine how different quite ordinary emotions become, and often how useful, if we do not identify with them."
+  },
+  {
+    "author": "P. D. Ouspensky",
+    "book": "The Fourth Way",
+    "quote": "Struggle with associative thinking is a definite line of work. If you study something, listen to something or are trying to understand something, you have to do it with a free mind. If you go on with your own thoughts at the same time, your mind will never be free to follow."
+  },
+  {
+    "author": "Robert Burton",
+    "book": "Self-Remembering",
+    "quote": "Almost everything is a buffer to self-remembering. It’s much easier to be in imagination, to be identified or to be negative than to self-remember. They are all convenient substitutes. Since self-remembering is not mechanical, it is very difficult. Anything is a deviation if one is not self-remembering."
+  },
+  {
+    "author": "Robert Burton",
+    "book": "Self-Remembering",
+    "quote": "After many years working with the system and Influence C, no matter what the I’s say, I have learned not to believe them, and to let them pass. One always looks for new I’s that would undermine one’s work; one does not become careless. When you verify that the machine is not real, you will be propelled to remember yourself."
+  },
+  {
+    "author": "Robert Burton",
+    "book": "Self-Remembering",
+    "quote": "What is it to be a man, but to struggle against the current of the masses, and through great labor and desolate trials create one’s soul? One begins with self-remembering in the morning and finishes with it at night. One can never afford to lay self-remembering aside; one ceases to exist."
+  },
+  {
+    "author": "Robert Burton",
+    "book": "Self-Remembering",
+    "quote": "Students who enter the way work at a full pace. Never lay self-remembering aside. Consciousness cannot be given, it must be earned through one’s own efforts."
+  },
+  {
+    "author": "Robert Burton",
+    "book": "Self-Remembering",
+    "quote": "To evolve one must transform the little negative events that occur each day; one can overcome great suffering only by transforming small sufferings. It is wonderful when one can see friction as an opportunity. It is such a strange lifesaver we have been cast by higher forces—the lifesaver of transforming suffering."
   }
 ]
